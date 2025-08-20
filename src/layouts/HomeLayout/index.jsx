40401e133eeee215adccfa04ecfe5a2c5ecb2320
tofayel <tofayeltuhin143@gmail.com>
2025-08-20 16:28:26 +0600

    fix layoput issue

--- src/layouts/HomeLayout/index.jsx
+++ src/layouts/HomeLayout/index.jsx
@@ -1,3 +1,20 @@
+// import Navbar from "@/components/main/shared/Navbar";
+// import ReactLenis from "lenis/react";
+// import { Outlet } from "react-router";
+
+// export default function HomeLayout() {
+//   return (
+//     <ReactLenis root>
+//       <div className="min-h-screen bg-[#EEF0F4] dark:bg-black flex flex-col">
+//         <Navbar />
+//         <main className="flex-1 py-5">
+//           <Outlet />
+//         </main>
+//       </div>
+//     </ReactLenis>
+//   );
+// }
+
 import Navbar from "@/components/main/shared/Navbar";
 import ReactLenis from "lenis/react";
 import { Outlet } from "react-router";
@@ -5,9 +22,16 @@ import { Outlet } from "react-router";
 export default function HomeLayout() {
   return (
     <ReactLenis root>
-      <div className="bg-[#EEF0F4] dark:bg-black h-full py-5">
-        <Navbar />
-        <Outlet />
+      <div className="min-h-screen bg-[#EEF0F4] dark:bg-black">
+        {/* Sticky Navbar */}
+        <header className="sticky top-0 z-50">
+          <Navbar />
+        </header>
+
+        {/* Main content */}
+        <main className="py-5">
+          <Outlet />
+        </main>
       </div>
     </ReactLenis>
   );
